7a4f494765cd0b9d388f4632f908446960de9de3
Opeyemi Omotayo <96225514+Opeyemi-Omotayo@users.noreply.github.com>
2024-09-19 15:49:17 +0100

    feat: added all colours and constant

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,25 +3,48 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-export const primary = '#2F50C1';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
+export const primary = "#2F50C1";
+
+const whatsappGreen = "#26d366";
+
+const everSongOrange100 = "#FFF3D5";
+const everSongOrange600 = "#DB7E21";
 
 export const Colors = {
+  primary: "#2F50C1",
+  blue100: "#E6E9F0",
+  blue200: "#6e91ec",
+  blue600: "#2F50C1",
+  lilac50: "#f9f8fb",
+  lilac100: "#F4F2F8",
+  lilac200: "#EAE7F2",
+  lilac400: "#A7A3B3",
+  lilac500: "#757281",
+  lilac600: "#58536E",
+  lilac700: "#3F395C",
+  red100: "#FEE3D4",
+  red600: "#D12030",
+  green100: "#E3FAD6",
+  green500: "#26d366",
+  green600: "#208D28",
+  orange100: "#FFF3D5",
+  orange600: "#DB7E21",
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#11181C",
+    background: "#fff",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#ECEDEE",
+    background: "#151718",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
   },
 };
